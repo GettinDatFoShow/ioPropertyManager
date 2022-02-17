fe5ac84c1368f0cd31df36db51841c888e464baa
GettinDatFoShow <robert.dale.morris@gmail.comn>
2022-02-17 13:21:50 -0500

    sign-up changes

--- src/app/pages/sign-up/sign-up.page.ts
+++ src/app/pages/sign-up/sign-up.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'iopm-sign-up',
@@ -9,20 +9,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SignUpPage implements OnInit {
 
   userSelection: boolean = false;
-  isOwner: boolean = true;
-  signUpForm: FormGroup;
+  isOwner: boolean = null;
+  public signUpForm: FormGroup;
+  companyForm: FormGroup;
+  companyNames: string[] = ['KB Homes'];
   
   constructor(private _fb: FormBuilder) { 
     
   }
 
   ngOnInit() {
+    // this.signUpForm = new FormGroup({
+    //   email: new FormControl(['', [Validators.required, Validators.email]]),
+    //   name: new FormControl(['', [Validators.required]]),
+    //   membership: new FormControl(['', [Validators.required]]),
+    //   companyName: new FormControl(['', [Validators.required]]),
+    //   phone: new FormControl(['', [Validators.required, Validators.minLength(10)]]),
+    //   password: new FormControl(['', [Validators.required]]),
+    //   confirm_password: new FormControl(['', [Validators.required]]),
+    // })
     this.signUpForm = this._fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      signInEmail: ['', [Validators.required, Validators.email]],
       name: ['', [Validators.required]],
-      membership: ['', [Validators.required, Validators.minLength(8)] ],
-      companyName: [''],
-      phone: ['', [Validators.required, Validators.pattern('^[1-9]\d{2}-\d{3}-\d{4}')]]
+      membership: ['', [Validators.required]],
+      companyName: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.minLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(10)]],
+      confirm_password: ['', [Validators.required]],
     })
   }
 
@@ -30,7 +43,32 @@ export class SignUpPage implements OnInit {
     console.log(event)
     this.isOwner = event.detail.value === 'owner';
     this.userSelection = true
+
     this.signUpForm.setValue({membership: event.detail.value});
+    console.log(this.signUpForm)
+    if (this.isOwner) {
+      this.companyForm = this._fb.group({
+
+      })
+    }
+  }
+
+  passwordConfirming(control: AbstractControl): { invalid: boolean } {
+    if (control.get('password').value !== control.get('confirm_password').value) {
+        return {invalid: true};
+    }
+  }
+
+  get signUpEmail() {
+    return this.signUpForm.get('signUpEmail');
+  }
+
+  get password() {
+    return this.signUpForm.get('passwords').get('password');
+  }
+
+  get confirm_password() {
+    return this.signUpForm.get('passwords').get('confirm_pasword')
   }
 
 }
