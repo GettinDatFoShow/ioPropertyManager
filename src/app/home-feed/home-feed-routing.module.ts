140bbb5b00559148e0ce87e5fe03b26cc118d14c
gettindatfoshow <robert.dale.morris@gmail.com>
2022-03-22 16:01:57 -0400

    added property modal

---
+++ src/app/home-feed/home-feed-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeFeedPage } from './home-feed.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeFeedPage,
+    children: [
+      {
+        path: 'homeFeed'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomeFeedPageRoutingModule {}
