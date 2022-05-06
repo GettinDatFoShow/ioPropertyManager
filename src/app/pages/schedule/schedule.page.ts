9190d8bb234ac92d7e75087b3d22cf5fcd383743
robert morris <robert.dale.morris@gmail.com>
2022-05-06 18:23:55 -0400

    added user load back up to schedule page

--- src/app/pages/schedule/schedule.page.ts
+++ src/app/pages/schedule/schedule.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
-import { AddScheduleDateComponent } from 'src/app/components/add-schedule-date/add-schedule-date.component';
+import { AddScheduleDateComponent } from '../../components/add-schedule-date/add-schedule-date.component';
+import { Company, User } from '../../global/models/globals.model';
+import { CompanyService } from '../../services/company/company.service';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'iopm-schedule',
@@ -15,10 +18,33 @@ export class SchedulePage implements OnInit {
   exDate1: Date = new Date('Mon Jun 13 2022 09:00:00 GMT-0400');
   exDate2: Date = new Date('Fri Jun 24 2022 10:00:00 GMT-0400');
   exDate3: Date = new Date('Thu Jul 28 2022 13:00:00 GMT-0400');
-
+  user: User = null;
+  company: Company = null;
   
-  constructor(public popoverController: PopoverController){}
+  constructor(private popoverController: PopoverController, private userService: UserService, private companyService: CompanyService) {
+    this.userService.userInfoChangeSub.subscribe((user)=>{
+      this.getUser();
+    });
+   }
 
+  ionViewWillEnter() {
+    this.getUser();
+  }
+
+  getUser() {
+    this.user = this.userService.getCurrentUser();
+    console.warn('home feed user');
+    console.warn(this.user)
+    this.user === null ? this.userService.reloadUser(): this.getUserCompanyInfo(this.user.companyId);
+  }
+
+  getUserCompanyInfo(cid: string) {
+    this.companyService.getCompany(cid).subscribe((company: Company)=> {
+      this.company = company;
+      this.companyService.unlockTabs(company);
+      // this.loadingUserData = false;
+    })
+  }
   ngOnInit() {
     console.log(this.today);
   }
